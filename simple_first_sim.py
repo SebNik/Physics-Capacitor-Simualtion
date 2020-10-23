c3d2c6184248c2643a45c444d94f304f22291500
SebNik <lord.voldmeort1492@gmail.com>
2020-10-23 20:45:13 +0200

    build getter and setter and converting vector in agles

--- simple_first_sim.py
+++ simple_first_sim.py
@@ -1,7 +1,10 @@
 # this file is simulating a simple grid model with two parallel capacitors
 # loading in modules
 import uuid
-from scipy.constants import pi, elementary_charge
+import numpy as np
+from scipy.constants import pi
+# physical_constants[name] = (value, unit, uncertainty)
+from scipy.constants import physical_constants as physical_constants
 
 
 class Electron:
@@ -12,9 +15,50 @@ class Electron:
         self.__x = x
         self.__y = y
         # setting charge
-        self.__charge = -1 * elementary_charge
+        self.__charge = -1 * physical_constants["elementary charge"][0]
         # setting id for identification
         self._id = uuid.uuid4()
+        # setting factor k for force cal
+        self.__k = 14 * pi * physical_constants['vacuum electric permittivity'][0]
+
+    def convert_vector_degrees(self, vector):
+        # converting vector  into degrees by calculating degrees in radians from horizontal vector
+        horizontal_vector = np.array([0, 1])
+        vector = np.array(vector)
+        # calculating a vector for unit
+        unit_vector_horizontal = horizontal_vector / np.linalg.norm(horizontal_vector)
+        unit_vector = vector / np.linalg.norm(vector)
+        dot_product = np.dot(unit_vector_horizontal, unit_vector)
+        # finding angles in vectors
+        angle = np.arccos(dot_product)
+        # returning angle
+        return angle
+
+    def cal_force(self):
+
+    def get_x(self):
+        # getting x coordinate
+        return self.__x
+
+    def set_x(self, x):
+        # setting the x coordinates
+        self.__x = x
+
+    def get_y(self):
+        # getting y coordinate
+        return self.__y
+
+    def set_y(self, y):
+        # setting the y coordinates
+        self.__y = y
+
+    def get_id(self):
+        # getting the id from the electron
+        return self._id
+
+    def get_charge(self):
+        # get the charge of the electron
+        return self.__charge
 
     def __repr__(self):
         # printing name of class
@@ -39,4 +83,6 @@ if __name__ == "__main__":
     e = Electron(x=5, y=2)
     # printing all information about it
     print(e)
-
+    # getting values
+    print("Coordinates: x: ", e.get_x(), "| y: ", e.get_y())
+    print("ID of electron: ", e.get_id(), " , charge: ", e.get_charge())
