be7b9510e0c85249600c9c1835191d56fa0bca9d
SebNik <lord.voldmeort1492@gmail.com>
2020-10-23 18:57:28 +0200

    getting information of instances

--- simple_first_sim.py
+++ simple_first_sim.py
@@ -1,5 +1,6 @@
 # this file is simulating a simple grid model with two parallel capacitors
 # loading in modules
+import uuid
 from scipy.constants import pi, elementary_charge
 
 
@@ -11,9 +12,27 @@ class Electron:
         self.__x = x
         self.__y = y
         self.__charge = -1 * elementary_charge
+        self._id = uuid.uuid4()
+
+    def __repr__(self):
+        # printing name of class
+        return "The class handles all actions of electrons"
+
+    def __str__(self):
+        # printing th object out for information
+        return "This is electron: {0}, with the charge: {1}, and the coordinates, x: {2} and y: {3}".format(self._id,
+                                                                                                            self.__charge,
+                                                                                                            self.__x,
+                                                                                                            self.__y)
+
+    def __del__(self):
+        # deleting function information
+        return "Deleting instance: " + self
 
 
 if __name__ == "__main__":
+    # printing class info
+    print(Electron)
     # setting the first single electron
     e = Electron(x=5, y=2)
     # printing all information about it
