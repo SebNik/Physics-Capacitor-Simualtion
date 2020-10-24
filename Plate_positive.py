b83f913ea73a97f430a5574fcf6bc63b51e1cc15
SebNik <lord.voldmeort1492@gmail.com>
2020-10-24 12:44:12 +0200

    adding the partcles in matrix for postive plane

--- Plate_positive.py
+++ Plate_positive.py
@@ -1,9 +1,10 @@
 # this file is working with the positive plate
 import uuid
 import numpy as np
+from Particle import Particle
 
 
-class Plate_positive:
+class Plate_Positive:
     # this is a class which represents one positive plate
     # here are all the protons fixed anc can't move
     def __init__(self, n, p1, p2, random=False):
@@ -14,9 +15,35 @@ class Plate_positive:
         self._p2 = p2
         self._p3 = [p1[0], p2[1], p1[2]]
         self._p4 = [p2[0], p1[1], p1[2]]
-        print("Bounding box point: ", self._p1, self._p2, self._p3, self._p4)
+        # print("Bounding box point: ", self._p1, self._p2, self._p3, self._p4)
+        # setting the z plane
+        self.z_plane = self._p1[2]
         # setting number of particles
         self._n = n
+        # setting the matrix
+        self.matrix = []
+        self.matrix_pos = []
+        # setting coordinates for the spacing of the particles
+        if not random:
+            x_ps = np.linspace(min(self._p1, self._p2)[0], max(self._p1, self._p2)[0], self._n)
+            y_ps = np.linspace(min(self._p1, self._p2)[1], max(self._p1, self._p2)[1], self._n)
+            # print("The positions for the spacing particles: ", x_ps, y_ps)
+            # iterating through positions
+            for x in x_ps:
+                row = []
+                data = []
+                for y in y_ps:
+                    # print("coordinates: ", x, y)
+                    row.append([x, y])
+                    data.append(Particle(x=x, y=y, z=self.z_plane, type_c='+'))
+                # adding the data into the matrix
+                self.matrix.append(data)
+                self.matrix_pos.append(row)
+            # setting the list in array
+            self.matrix = np.array(self.matrix)
+            self.matrix_pos = np.array(self.matrix_pos)
+
+        # print(self.matrix, '\n\n', self.matrix_pos)
 
     def __repr__(self):
         # printing name of class
@@ -24,15 +51,14 @@ class Plate_positive:
 
     def __str__(self):
         # printing th object out for information
-        return "This is a Plate : {0}, with a positive charge. The bounding box coordinates are: p1: {1}, p2: {2}, p3: {3}, p4: {4}".format(
-            self._id, self._p1, self._p2, self._p3, self._p4)
+        return "This is a Plate : {0}, with a positive charge. The bounding box coordinates are: p1: {1}, p2: {2}, p3: {3}, p4: {4}, on the z-plane: {5}".format(self._id, self._p1, self._p2, self._p3, self._p4, self.z_plane)
 
 
 if __name__ == "__main__":
     # getting class information
-    print(Plate_positive)
+    print(Plate_Positive)
     # setting instance of single plate
-    plate_pos = Plate_positive(n=3, p1=[0, 0, 0], p2=[3, 3, 0])
+    plate_pos = Plate_Positive(n=3, p1=[0, 0, 0], p2=[1, 1, 0])
     # printing all information about it
     print(plate_pos)
     # getting values
