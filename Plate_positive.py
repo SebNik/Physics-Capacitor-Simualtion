32b077729a8f58e1e22a01c8f36fe925c8bf7db1
SebNik <lord.voldmeort1492@gmail.com>
2020-10-24 12:06:11 +0200

    adding printing out for the instance

--- Plate_positive.py
+++ Plate_positive.py
@@ -1,4 +1,5 @@
 # this file is working with the positive plate
+import uuid
 import numpy as np
 
 
@@ -6,12 +7,25 @@ class Plate_positive:
     # this is a class which represents one positive plate
     # here are all the protons fixed anc can't move
     def __init__(self, n, p1, p2, random=False):
+        # setting an id for the plate
+        self._id = uuid.uuid4()
         # setting bounding box edges
-        p3 = [p1[0], p2[1], p1[2]]
-        p4 = [p2[0], p1[1], p1[2]]
-        print(p1, p2, p3, p4)
+        self._p1 = p1
+        self._p2 = p2
+        self._p3 = [p1[0], p2[1], p1[2]]
+        self._p4 = [p2[0], p1[1], p1[2]]
+        print("Bounding box point: ", self._p1, self._p2, self._p3, self._p4)
         # setting number of particles
         self._n = n
+
+    def __repr__(self):
+        # printing name of class
+        return "The class handles all actions of a single Plate"
+
+    def __str__(self):
+        # printing th object out for information
+        return "This is a Plate : {0}, with a positive charge. The bounding box coordinates are: p1: {1}, p2: {2}, p3: {3}, p4: {4}".format(
+            self._id, self._p1, self._p2, self._p3, self._p4)
 
 
 if __name__ == "__main__":
