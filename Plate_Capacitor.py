ae987081e1d61fcb4c0003c4b65f1410f416b3b4
SebNik <lord.voldmeort1492@gmail.com>
2020-10-25 11:56:16 +0100

    plotting force vectors

--- Plate_Capacitor.py
+++ Plate_Capacitor.py
@@ -37,6 +37,36 @@ class Plate_Capacitor:
         # returning all vales
         return force_list, force_dic
 
+    def plotting_plates_vectors_force(self):
+        # plotting the 3D room of the electrons and their vectors
+        # getting force vectors
+        force_l, force_dic = self.cal_forces()
+        # setting up space
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        # plotting points of pos plate
+        for e_p in self.plate_pos.matrix.flatten():
+            ax.scatter3D(e_p.get_x(), e_p.get_y(), e_p.get_z(), c='r')
+        # plotting points of neg plate
+        for e_n in self.plate_neg.matrix.flatten():
+            ax.scatter3D(e_n.get_x(), e_n.get_y(), e_n.get_z(), c='b')
+            ax.quiver(e_n.get_x(), e_n.get_y(), e_n.get_z(), force_dic[str(e_n.get_id())][0],
+                      force_dic[str(e_n.get_id())][1], force_dic[str(e_n.get_id())][2])
+        # plotting the plates for better view
+        r = [self._p1[0], self._p2[0]]
+        x, y = np.meshgrid(r, r)
+        # plotting the pos plate
+        pos_z = np.full((2, 2), self.plate_pos.z_plane)
+        ax.plot_surface(x, y, pos_z, alpha=0.5, color='r')
+        # plotting the neg plate
+        neg_z = np.full((2, 2), self.plate_neg.z_plane)
+        ax.plot_surface(x, y, neg_z, alpha=0.5, color='b')
+        # setting labels
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        # plotting out the room
+        plt.show()
 
     def plotting_plates(self):
         # plotting the 3D room of the electrons
@@ -68,10 +98,12 @@ class Plate_Capacitor:
 
 if __name__ == "__main__":
     # setting up an instances for test
-    cap = Plate_Capacitor(n=3, p1=[0, 0], p2=[0.001, 0.001], plane_z_pos=[0], plane_z_neg=[0.001], random=False)
+    cap = Plate_Capacitor(n=3, p1=[0, 0], p2=[1e-15, 1e-15], plane_z_pos=[0], plane_z_neg=[1e-15], random=False)
     # cap.plate_neg.get_inner_forces()
     # plotting the room
     # cap.plotting_plates()
     # getting the forces for the particles
     print(cap.cal_forces())
     # print(cap.plate_neg.matrix[0][0].get_id())
+    # plotting forces
+    cap.plotting_plates_vectors_force()
