76bf545d53cb99a46ae3a40ecaa0df3224d4a3b3
SebNik <lord.voldmeort1492@gmail.com>
2020-10-28 19:20:56 +0100

    fixed z plane

--- Open_Pickle.py
+++ Open_Pickle.py
@@ -1,15 +1,39 @@
 # this file is opening saved classes
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 from Plate_Capacitor import Plate_Capacitor
 
 if __name__ == "__main__":
-    # setting a path
-    path = 'D:\\Python\\Programme\\Physics-Capacitor-Simualtion\\resources\\exports\\27_10_2020__23_35_54\\class.pickle'
-    # path open an old pickle file
-    cap = pickle.load(open(path, "rb", -1))
+    # # setting a path
+    # path = 'D:\\Python\\Programme\\Physics-Capacitor-Simualtion\\resources\\exports\\27_10_2020__23_35_54\\class.pickle'
+    # # path open an old pickle file
+    # cap = pickle.load(open(path, "rb", -1))
     # print(cap.plate_neg.plot_density())
-    e_field, length = cap.cal_electric_field()
-    np.savez_compressed('resources/exports/27_10_2020__23_35_54/arr.npz', e_field)
-    e_field_max = np.where(e_field[:, 3] == e_field[:, 3].max())[0][0]
+    # e_field, length = cap.cal_electric_field()
+    # np.savez_compressed('resources/exports/27_10_2020__23_35_54/arr.npz', e_field)
+    # e_field_max = np.where(e_field[:, 3] == e_field[:, 3].max())[0][0]
     # print(e_field[e_field_max])
+    e_field = np.load('resources/exports/27_10_2020__23_35_54/arr.npz')['arr_0']
+    resolution = 40
+    # reading in z planes offsets
+    z = []
+    for t in e_field:
+        if t[2] not in z:
+            z.append(t[2])
+    print(z)
+    for off in z:
+        # data 2d plot
+        data_2d_plot = []
+        for r in e_field:
+            if r[2] == off:
+                data_2d_plot.append(r)
+        data_2d_plot = np.array(data_2d_plot)
+        a = data_2d_plot[data_2d_plot[:, 2].argsort()]  # First sort doesn't need to be stable.
+        a = a[a[:, 1].argsort(kind='mergesort')]
+        a = a[a[:, 0].argsort(kind='mergesort')]
+        # image plotting
+        image = a[:, 3].reshape(int(len(a) / int(resolution)), int(resolution))
+        fig, ax = plt.subplots()
+        ax.imshow(image)
+        plt.show()
